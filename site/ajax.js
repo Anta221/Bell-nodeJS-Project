44c924ec959253267d2044f418dc97ddff8338a3
anta <antandoye0@gmail.com>
2020-03-21 19:09:00 +0100

    template-add-vote

--- site/ajax.js
+++ site/ajax.js
@@ -1,17 +1,15 @@
 class Ajax{
-  constructor(lien, name, urlSound){
+  constructor(lien){
     this.lien = lien;
-    this.name = name;
-    this.urlSound = urlSound;
   }
   /**
    * Ajouter Une sonnerie 
    */
-  addSound(){
+  addSound(name, urlSound){
           let erreur = true; 
   
             //vérification champ name
-            if(this.name == ""){
+            if(name == ""){
               $("#erreur_name").text("Veuillez saisir le Nom");
               $("#valider").prop("disabled" , true);
               erreur = false; 
@@ -19,7 +17,7 @@ class Ajax{
             }
   
             //vérification champ lien
-            if(this.urlSound == ""){
+            if(urlSound == ""){
               $("#erreur_lien").text("Veuillez saisir le lien");
               $("#valider").prop("disabled" , true);
               erreur = false; 
@@ -28,8 +26,8 @@ class Ajax{
            if(erreur == true){
   
               let donnee = {
-                  "name" : this.name, 
-                  "lien" : this.urlSound
+                  "name" : name, 
+                  "lien" : urlSound
               };
            
               $.post({
@@ -70,15 +68,10 @@ class Ajax{
    * Affichage des sonneries et systéme de vote 
    */
   getAndVoteSound(){
-      
-    
           $.get({
               url: this.lien, 
-              success:function(res){
-  
-                 
+              success:function(res){               
                    let tab =[];
-  
                   $.each( res, function( key, val ) {
                         
                          tab.push(val );
@@ -92,14 +85,14 @@ class Ajax{
                       html +='<div class="col-sm-4">'; 
                       html +='<div class="card">';
                       html +='<div class="embed-responsive embed-responsive-16by9">'
-                      //html += '<iframe class="embed-responsive-item" src="'+tab[i].lien+'" allowfullscreen></iframe>';
                       html += '<iframe src="'+tab[i].lien+'" frameborder="1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
                       html +='</div>';
                       html +='<div class="card-body">';                   
                       html +='<h5 class="card-title"></h5>';
                       html +='<p class="card-text"> Nom: '+ tab[i].name +'</p>';
                       html +='<p class="card-text"> URL: <a href="'+ tab[i].lien +'" target="_blank">' + tab[i].name +'</a></p>';
-                      html +='<p class="coeur d-flex">&#9829;<span><span></p>';
+                      html +='<p class="coeur d-flex col-sm">&#9829;<span></span> <input type="hidden" value=" '+tab[i]._id  +' "></p>';
+                      html +='<p class="card-text"> Note: '+ tab[i].note +'</p>';
                       html +='</div>';
                       html +='</div>';
   
@@ -111,31 +104,41 @@ class Ajax{
                     $("#sonnerie .row").html(tab2);
   
   
+                    //compteurs des votes et enregistrement du nombre de vote 
                     let coeurs = document.querySelectorAll(".coeur"); 
                     let cpt = []; 
-                      for(let i = 0 ; i < coeurs.length ; i++){
-                          cpt[i] = 0; 
-                          coeurs[i].addEventListener("click" , function(e){
-                              cpt[i] ++;
-                              console.log(cpt);
-                          
-                              e.currentTarget.childNodes[1].innerHTML = cpt[i] ; 
-  
-                              console.log(  e.currentTarget);
-                          })
-                          
-                      }
-                      
-                      
-  
-                 
-              }
+                    for(let i = 0 ; i < coeurs.length ; i++){
+                        cpt[i] = 0; 
+                        coeurs[i].addEventListener("click" , function(e){
+                        cpt[i] ++;
+                        e.currentTarget.childNodes[1].innerHTML = cpt[i] ; 
+                        let id_bell = e.currentTarget.childNodes[3].value;
+
+                        let note = {'note' :  cpt[i]};
+                        let url = "http://127.0.0.1:3000/bells/"+id_bell ; 
+                        let newUrl = url.replace("%20" , "");
+                        let uri = newUrl.replace(/ /g,'');
+                        $.ajax({
+                        url: uri,
+                        type: "PUT",
+                        data: note,
+                        success(res){
+                          console.log(res)
+                          },
+                        error(err){
+                          console.log(err)
+                          } 
+                        });
+                      });
+                    }
+                  }
           });
   
             
      
       
   }
+
 
   /**
    * Contrôle des inputs 
@@ -176,33 +179,38 @@ class Ajax{
 
 
 $(document).ready(function() {
+  var addsound = new Ajax("http://127.0.0.1:3000/bell");
 
-      sound = new Ajax("http://127.0.0.1:3000/bell",$("#name").val(), $("#lien").val());
-    
       // Permet d'ajouter une sonnerie
       $("#valider").on("click" , function(){
-        sound.addSound();
+        var name = $("#name").val(); 
+        var lien = $("#lien").val();
+     
+        
+        addsound.addSound(name, lien);
+
       })
-  
+   
 
       //contrôle des inputs
       $("#name").on("change" , function(){
-        sound.controlName($("#name").val()); 
+        addsound.controlName($("#name").val()); 
       })
 
       //controle type url
       $("#lien").on("change" , function(){
-        sound.controlLink($("#lien").val());
+        addsound.controlLink($("#lien").val());
       })
   
   
       //Affiche toutes les sonneries & Permet de voter
       $("#vote").on('click' , function(){
-        sound.getAndVoteSound();
+        addsound.getAndVoteSound();
+        //addsound.updateBell();
       }); 
   
   
-  
+   
       // bouton  Retour
       $("#btn-retour").on("click" , function(){
         window.location=('index.html');
